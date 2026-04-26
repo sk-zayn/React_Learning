065dd912bfaec3bb59ad800b56360e077f1ec72f
Sadik Shaikh <skzain007995@gmail.com>
2026-04-26 10:16:36 +0530

    Refactor Todo component to manage local state for todo items and input text

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 
 function Todo(props) {
-  const todoItems = props.todoList.map((todo)=> <TodoItem  key={todo.id} todo={todo}/>)
-  return <ul> {todoItems} </ul>
+  // const todoItems = props.todoList.map((todo)=> <TodoItem  key={todo.id} todo={todo}/>)
+  const [todoItems, setTodoItems] = useState([{data: "Todo 1" , id: 201}, {data: "Todo 2", id: 202}]);
+  const [inputText, setInputText]= useState("")
+  return(
+    <>
+    <input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)}></input>
+    <button onClick={()=>setTodoItems([...todoItems, {data: inputText, id: (new Date()).getTime()}])}>Add Todo</button>
+      <ul> {todoItems.map((todo) => <TodoItem todo = {todo} key={todo.id} />)} </ul>
+    </>
+  );
 
 };
 
