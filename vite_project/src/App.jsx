7f9a11f944b9a5d17dafc57a3e73afe3268a37ea
Zain Shaikh <skzain007995@gmail.com>
2026-04-29 20:25:40 +0530

    feat: initialize Vite project with React setup
    - Add package.json with dependencies and scripts for development, build, linting, and preview.
    - Create App component with a simple counter functionality.
    - Add CSS file for styling the App component.

---
+++ vite_project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  function countHandler(){
+    setCount(count+1);
+  };
+  return (
+    <div>
+        <button onClick={countHandler}> Counter {count} </button>
+    </div>
+  )
+}
+
+
+export default App
