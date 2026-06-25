0bc8cbd337c6c5689510b23912522e0c2edf70ac
Zain Shaikh <skzain007995@gmail.com>
2026-06-25 23:30:22 +0530

    added counter component and refactor code

---
+++ Hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Counter from '../../React_Learning/src/Counter'
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
---
+++ Hooks/src/components/Counter.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [num, setNum] = useState(0);
+
+  const increment = () => {
+    setNum(num + 1);
+  };
+
+  const decrement = () => {
+    setNum(num - 1);
+  };
+
+  return (
+    <div className="flex flex-col p-10 gap-10 w-90 text-center font-bold text-3xl">
+      <div className="bg-gray-300 p-8 ">{num}</div>
+      <div className="flex gap-15">
+        <button
+          onClick={() => increment()}
+          className="bg-blue-600 px-10 py-3 text-white rounded-full"
+        >
+          +
+        </button>
+        <button onClick={()=> decrement()} className="bg-blue-600 px-10 py-3 text-white rounded-full">
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
