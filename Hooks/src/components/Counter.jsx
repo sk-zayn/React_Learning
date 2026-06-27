d53db6ab67366ddc1c3f9fce824598ba0a57a419
Zain Shaikh <skzain007995@gmail.com>
2026-06-27 23:51:30 +0530

    feat: add dark mode toggle and improve styling in Counter component

--- Hooks/src/components/Counter.jsx
+++ Hooks/src/components/Counter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [num, setNum] = useState(0);
+  const [dark, setDark] = useState(false);
 
   const increment = () => {
     setNum(num + 1);
@@ -11,13 +12,20 @@ const Counter = () => {
     setNum(num - 1);
   };
 
-  const reset = ()=>{
-  setNum(0)
-  }
+  const reset = () => {
+    setNum(0);
+  };
 
   return (
-    <div className="flex flex-col p-10 gap-10 w-90 text-center font-bold text-3xl">
-      <div className="bg-gray-300 p-8 ">{num}</div>
+    <div  style={{
+          background: dark ? "black" : "white",
+          color: dark ? "white" : "black",
+          minHeight: "100vh",
+          minwidth: "100vw",
+          padding: "30px",
+        }}
+        className="flex flex-col p-10 gap-10  text-center font-bold text-3xl">
+      <div className="bg-gray-300 p-8 w-120">{num}</div>
       <div className="flex gap-15">
         <button
           onClick={() => increment()}
@@ -25,13 +33,22 @@ const Counter = () => {
         >
           +
         </button>
-        <button onClick={()=> decrement()} className="bg-blue-600 px-10 py-3 text-white rounded-full">
+        <button
+          onClick={() => decrement()}
+          className="bg-blue-600 px-10 py-3 text-white rounded-full"
+        >
           -
         </button>
-        <button onClick={() => reset()} className="bg-blue-600 px-10 py-3 text-white rounded-full" >
-        Reset
-      </button>
+        <button
+          onClick={() => reset()}
+          className="bg-blue-600 px-10 py-3 text-white rounded-full"
+        >
+          Reset
+        </button>
       </div>
+
+        <button className="bg-blue-500 w-50 rounded-full" onClick={() => setDark(!dark)}>Change Theme</button>
+
     </div>
   );
 };
